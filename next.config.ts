77dcfbfeec3a37504ecc881acc477d7877291f17
HazmiHisham <hazmi2582000@gmail.com>
2026-07-03 16:57:45 +0800

    Fix Cloudflare deployment

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -13,6 +14,5 @@ const nextConfig: NextConfig = {
 
 export default nextConfig;
 
-import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 
 initOpenNextCloudflareForDev();
